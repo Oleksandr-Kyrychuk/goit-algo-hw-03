abb8e8240dc99f814c63a8f31b9b1c5fa91f51dd
Oleksandr-Kyrychuk <azion2342@gmail.com>
2024-09-15 16:39:06 +0300

    Update dz2.py

--- dz2.py
+++ dz2.py
@@ -6,13 +6,11 @@ quantity = 5
 
 
 def get_numbers_ticket(min, max, quantity):
-    if min>0 and max<1000 and quantity<=len(range(min,max+1)):
+    if min>0 and max<1000 and quantity<=len(range(min,max)):
         a = list(range(min,max+1))
         get_numbers_ticket = random.sample(a, quantity)
         return sorted(get_numbers_ticket)
     else:
-        get_numbers_ticket = []
-        print(sorted(get_numbers_ticket))
         return []
         
 
